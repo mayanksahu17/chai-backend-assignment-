58042167fc34b305f20b434553ba1b8bc10fef23
Mayank <mayank0real0world@gmail.com>
2024-02-18 23:12:12 +0530

    tweet controller add

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -6,19 +6,83 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
+ try {
+      const { content } = req.body
+      if (!content || typeof content !== 'string') throw new ApiError('Invalid Tweet Content', 400)
+      const tweet = await Tweet.create({
+       content ,
+       owner : req.user._id
+       }) 
+   
+       if (!tweet) {
+           throw new ApiError(401,"Server Error",)
+       }
+
+       res
+       .status(200)
+       .json(
+        new ApiResponse(
+            200,
+            tweet,
+            "tweet created successfully"
+            )
+       )
+ } catch (error) {
+    throw new ApiError(401,"Internal server  error")
+ }
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
-    // TODO: get user tweets
-})
+    try {
+        const userId = req.params.userId
+        const user  = await User.findById(userId)
+        const  tweets=await Tweet.find({owner:userId}).sort("-createdAt").populate({
+             path:"owner",
+        })
+        if (!tweets) {
+            throw new ApiError("No Tweets Found","Not found")   
+        }
+        res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                tweets,
+                "tweets  fetched Successfully"
+            )
+        )
+    } catch (error) {
+        throw new ApiError(500,'server error')
+    }
+}
+)
 
 const updateTweet = asyncHandler(async (req, res) => {
-    //TODO: update tweet
+   try {
+    const tweetId = req.params.tweetId
+    const {content} = req.body
+
+    const tweet = await Tweet.findByIdAndUpdate(tweetId ,{
+        content : content
+    })
+
+    res
+    .status(200)
+    .json(
+        new  ApiResponse(
+            200,
+            tweet,
+            'Updated successfully'
+        )
+    )
+
+   } catch (error) {
+    throw new ApiError(500,  'Server Error')
+   }
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
+    
 })
 
 export {
